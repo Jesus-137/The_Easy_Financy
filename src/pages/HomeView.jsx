09e15671448459382b1e30ef18f6a2d5b69be0cd
practica <velazquezhernandezjesus13@gmail.com>
2023-02-25 17:31:57 -0600

    Se creo la pagina home view

---
+++ src/pages/HomeView.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/molecules/Header";
+import Content from "../components/organims/Content";
+import Footer from "../components/molecules/Footer";
+
+function HomeView() {
+    return (
+        <>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </>
+    );
+}
+
+export default HomeView;
